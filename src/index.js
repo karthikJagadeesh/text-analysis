04915bed22ed667c1537ccf72308395dcca5a112
Karthik Jagadeesha <kartu8@gmail.com>
2017-11-13 20:27:40 +0530

    Setup module bundler

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import p5 from "p5";
+import nlp from "compromise";
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: "dist"
+  } 
+};
+
+module.exports = config;
