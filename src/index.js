55e95495e1190d253934f14cd628459fa71392ce
Karthik Jagadeesha <kartu8@gmail.com>
2017-11-14 22:43:17 +0530

    Add next button

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ import nlp from "compromise";
     let img = document.createElement("img");
     img.src = url;
     img.className = "news-image";
-
     return img;
   };
 
@@ -37,24 +36,41 @@ import nlp from "compromise";
     const rNo = Math.round(Math.random() * (result.length - 1));
     const news = result[rNo];
     console.log(`NEWS: \n`, nlp);
-    const negatedSentence = nlp(news.abstract).sentences().toNegative().out("text");
-    const futureTensed = nlp(negatedSentence).sentences().toFutureTense().out("text");
-    // const pluralised = nlp(futureTensed).nouns().toPlural().out("text");
+    const negatedSentence = nlp(news.abstract)
+      .sentences()
+      .toNegative()
+      .out("text");
+    const futureTensed = nlp(negatedSentence)
+      .sentences()
+      .toFutureTense()
+      .out("text");
+
     const imageUrl = news.multimedia.find(item => item.format === "Normal").url;
     let newsBody = document.querySelector("#news-body");
 
     const newsTitleNode = createPNode(futureTensed);
+    newsBody.children[1] ? newsBody.removeChild(newsBody.children[1]) : void 0;
+    newsBody.children[1] ? newsBody.removeChild(newsBody.children[1]) : void 0;
     newsBody.appendChild(newsTitleNode);
 
     const newsImageNode = createImgNode(imageUrl);
     newsBody.appendChild(newsImageNode);
   };
 
-  fetch(
-    nyAPI +
-      sections[Math.round(Math.random() * (sections.length - 1))] +
-      _apiKey
-  )
-    .then(response => response.json())
-    .then(response => newsRecieved(response.results));
+  const getFreshNews = () => {
+    fetch(
+      nyAPI +
+        sections[Math.round(Math.random() * (sections.length - 1))] +
+        _apiKey
+    )
+      .then(response => response.json())
+      .then(response => newsRecieved(response.results));
+  };
+
+  getFreshNews();
+
+  //User Events
+  document
+    .querySelector("#next-button")
+    .addEventListener("click", getFreshNews);
 })();
